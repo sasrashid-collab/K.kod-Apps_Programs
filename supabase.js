49062127040b5a8266b35fe53fdbbe69f190daaa
ئابى كوردى بو كورد <sas.rashid@gmail.com>
2026-02-15 03:02:33 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,7 @@
-<script type="module">
+// supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
 export const supabase = createClient(
   "https://jghcjxlwiizprsmuvnoz.supabase.co",
   "sb_publishable_a_xw7b8TnitjSB6MCOC3Pg_T0NB15bO"
 )
-</script>
-
