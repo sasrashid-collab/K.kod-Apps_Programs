ac94f4528854d61138b426952554e0ef39e58f9e
ئابى كوردى بو كورد <sas.rashid@gmail.com>
2026-02-15 18:35:15 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,3 +5,8 @@ export const supabase = createClient(
   "https://jghcjxlwiizprsmuvnoz.supabase.co",
   "sb_publishable_a_xw7b8TnitjSB6MCOC3Pg_T0NB15bO"
 )
+<footer style="text-align:center; font-size:14px; margin-top:40px; color:#555;">
+  <a href="idea-protection.html" target="_blank" style="color:#2563eb; text-decoration:none;">
+    سیاسەتی پاراستنی بیرۆکە و متمانە
+  </a>
+</footer>
